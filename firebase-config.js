b352fa3c96898b0fc8e62e75ee74a5f3d5b2d41e
siddiquijibran01-dotcom <siddiquijibran01@gmail.com>
2025-08-19 13:47:23 +0530

    Update and rename firebase-config.sample.js to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA9vmRlUsHetjuD7L1l-DxtQwn7WPW6T7I",
+  authDomain: "golden-green-recipe.firebaseapp.com",
+  projectId: "golden-green-recipe",
+  storageBucket: "golden-green-recipe.firebasestorage.app",
+  messagingSenderId: "952103785525",
+  appId: "1:952103785525:web:06a9eeb8d33364446e4d7c"
+};
