d69e69a7f30677b76a7d8c19c21b3267c2611af8
siddiquijibran01-dotcom <siddiquijibran01@gmail.com>
2025-08-19 14:44:00 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,129 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA9vmRlUsHetjuD7L1l-DxtQwn7WPW6T7I",
-  authDomain: "golden-green-recipe.firebaseapp.com",
-  projectId: "golden-green-recipe",
-  storageBucket: "golden-green-recipe.firebasestorage.app",
-  messagingSenderId: "952103785525",
-  appId: "1:952103785525:web:06a9eeb8d33364446e4d7c"
-};
+{
+  "project_info": {
+    "project_number": "822648275612",
+    "project_id": "meatque-7ec10",
+    "storage_bucket": "meatque-7ec10.firebasestorage.app"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:822648275612:android:44e8095cda6330d62ce359",
+        "android_client_info": {
+          "package_name": "com.mt.meatque"
+        }
+      },
+      "oauth_client": [
+        {
+          "client_id": "822648275612-icrsob4cb0e6ecbcacp2gs2hombondi0.apps.googleusercontent.com",
+          "client_type": 1,
+          "android_info": {
+            "package_name": "com.mt.meatque",
+            "certificate_hash": "6d106af6168490ed1c34d57746be7bdd453810a7"
+          }
+        },
+        {
+          "client_id": "822648275612-ig4nvk5u2f6fennoqtqbnubvstf19b0j.apps.googleusercontent.com",
+          "client_type": 1,
+          "android_info": {
+            "package_name": "com.mt.meatque",
+            "certificate_hash": "9afbc36437333728519f2a891ed68680424c67c2"
+          }
+        },
+        {
+          "client_id": "822648275612-hq8sqs7sgdn1vgeiqlqa44qq07hu7kou.apps.googleusercontent.com",
+          "client_type": 3
+        }
+      ],
+      "api_key": [
+        {
+          "current_key": "AIzaSyAst89yg-wyiVAzVKeV_JRDjEwGQRAj51M"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": [
+            {
+              "client_id": "822648275612-hq8sqs7sgdn1vgeiqlqa44qq07hu7kou.apps.googleusercontent.com",
+              "client_type": 3
+            }
+          ]
+        }
+      }
+    },
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:822648275612:android:ae775670632047792ce359",
+        "android_client_info": {
+          "package_name": "com.mt.meatque_delivery_boy"
+        }
+      },
+      "oauth_client": [
+        {
+          "client_id": "822648275612-mnretrhi5qorgb18kege3f1prg9napgc.apps.googleusercontent.com",
+          "client_type": 1,
+          "android_info": {
+            "package_name": "com.mt.meatque_delivery_boy",
+            "certificate_hash": "6d106af6168490ed1c34d57746be7bdd453810a7"
+          }
+        },
+        {
+          "client_id": "822648275612-hq8sqs7sgdn1vgeiqlqa44qq07hu7kou.apps.googleusercontent.com",
+          "client_type": 3
+        }
+      ],
+      "api_key": [
+        {
+          "current_key": "AIzaSyAst89yg-wyiVAzVKeV_JRDjEwGQRAj51M"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": [
+            {
+              "client_id": "822648275612-hq8sqs7sgdn1vgeiqlqa44qq07hu7kou.apps.googleusercontent.com",
+              "client_type": 3
+            }
+          ]
+        }
+      }
+    },
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:822648275612:android:87f035602b1713e72ce359",
+        "android_client_info": {
+          "package_name": "com.mt.meatque_vendor"
+        }
+      },
+      "oauth_client": [
+        {
+          "client_id": "822648275612-0d82l6t0laeto72i077e9ugpmrlh9v2b.apps.googleusercontent.com",
+          "client_type": 1,
+          "android_info": {
+            "package_name": "com.mt.meatque_vendor",
+            "certificate_hash": "6d106af6168490ed1c34d57746be7bdd453810a7"
+          }
+        },
+        {
+          "client_id": "822648275612-hq8sqs7sgdn1vgeiqlqa44qq07hu7kou.apps.googleusercontent.com",
+          "client_type": 3
+        }
+      ],
+      "api_key": [
+        {
+          "current_key": "AIzaSyAst89yg-wyiVAzVKeV_JRDjEwGQRAj51M"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": [
+            {
+              "client_id": "822648275612-hq8sqs7sgdn1vgeiqlqa44qq07hu7kou.apps.googleusercontent.com",
+              "client_type": 3
+            }
+          ]
+        }
+      }
+    }
+  ],
+  "configuration_version": "1"
+}
